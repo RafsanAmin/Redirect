95098ca63650de0acfd345f7a45ac01cfc6e2b77
RafsanAmin <rafsanamin2020@gmail.com>
2022-11-11 12:38:00 +0600

    d

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/static/index.html');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Running');
+});
